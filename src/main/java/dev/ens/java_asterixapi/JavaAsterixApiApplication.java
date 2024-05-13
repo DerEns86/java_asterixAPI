c8a76bff16aac968c9944a88a6d95f8ba43bfeba
Viktor Ens <v.ens86@yahoo.de>
2024-05-13 10:52:58 +0200

    Initial commit

---
+++ src/main/java/dev/ens/java_asterixapi/JavaAsterixApiApplication.java
@@ -0,0 +1,13 @@
+package dev.ens.java_asterixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAsterixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAsterixApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ens/java_asterixapi/JavaAsterixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ens.java_asterixapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAsterixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
